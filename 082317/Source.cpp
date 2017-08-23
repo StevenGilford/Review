663e38166dfb12a8d3851234fed20e3238156dc1
Steven Gilford <spgwarriorcat@yahoo.com>
2017-08-23 15:05:51 -0500

    New project......
    For "practice"

---
+++ 082317/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+	float keepRunning[5] = {1.0f, 2.5f, 3.5f, 10.0f, 1.5f};
+	float a = keepRunning[0] + keepRunning[1] + keepRunning[2] + keepRunning[3] + keepRunning[4];
+
+	float amRunning[5] = {1.5, 2.0, 3.0, 10.5, 1.7};
+	float b = 0;
+	for (int i = 0; i < 5; i++)
+	{
+
+	}
+
+system("pause");
+}
